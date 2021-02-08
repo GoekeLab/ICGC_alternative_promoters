a052aa139a88693e9a4e6f2c5f5fd0d328cbdb21
Ubuntu <ywan6@alumni.jh.edu>
2021-02-08 02:07:13 +0000

    proActiv uses STAR junctions as inputs;add DEXSeq

--- bin/run_proactiv.r
+++ bin/run_proactiv.r
@@ -17,7 +17,7 @@
 ################################################
 ################################################
 library(proActiv)
-library(BSgenome.Hsapiens.UCSC.hg38) ## add fasta option to proActiv?
+library(DEXSeq)
 
 ################################################
 ################################################
@@ -35,11 +35,32 @@ samplesheet   <- strsplit(grep('--samplesheet*', args, value = TRUE), split = '=
 ################################################
 ################################################
 sample_tab <- read.csv(samplesheet,header=TRUE)
-files <- sample_tab$bam
+files <- sample_tab$input_file
 condition <- sample_tab$condition
 promoterAnnotation <- promoterAnnotation.gencode.v34.subset
-result <- proActiv(files = files, promoterAnnotation = promoterAnnotation,
-                   genome = 'hg38', condition = condition)
+result <- proActiv(files = files, condition = condition,
+                   promoterAnnotation = promoterAnnotation)
 result <- result[complete.cases(assays(result)$promoterCounts),]
 countData <- data.frame(assays(result)$promoterCounts, rowData(result))
-write.csv(countData,file = "proActiv_outputs.csv")
+write.table(countData, file = "proActiv_count.csv",
+            sep = ",", quote = FALSE, row.names = FALSE)
+
+################################################
+################################################
+## RUN DEXSeq for alternative promoter usage  ##
+################################################
+################################################
+library(DEXSeq)
+## Call DEXSeq - promoter as feature, gene as group
+dxd <- DEXSeqDataSet(countData = as.matrix(countData[,seq_len(length(condition))]),
+                     sampleData = data.frame(colData(result)),
+                     design = formula(~ sample + exon + condition:exon),
+                     featureID = as.factor(countData$promoterId),
+                     groupID = as.factor(countData$geneId))
+dxr1 <- DEXSeq(dxd)
+dxr1 <- data.frame(dxr1[,1:10]) %>% 
+  group_by(groupID) %>% 
+  mutate(minp = min(padj)) %>%
+  arrange(minp)
+write.table(dxr1, file = "DEXSeq_alt_promoter_usage_output.csv",
+            sep = ",", quote = FALSE, row.names = FALSE)
--- modules/process/proactiv.nf
+++ modules/process/proactiv.nf
@@ -11,8 +11,8 @@ process PROACTIV {
         mode: params.publish_dir_mode,
         saveAs: { filename -> saveFiles(filename:filename, options:params.options, publish_dir:getSoftwareName(task.process), publish_id:'') }
 
-    conda     (params.enable_conda ? "conda-forge::r-base=4.0.0 bioconda::bioconductor-proactiv=1.0.0 bioconda::bioconductor-bsgenome.hsapiens.ucsc.hg38=1.4.3" : null)
-    container "docker.io/yuukiiwa/proactiv:0.0"
+    conda     (params.enable_conda ? "conda-forge::r-base=4.0.0 bioconda::bioconductor-proactiv=1.0.0 bioconda::bioconductor-dexseq=" : null)
+    container "docker.io/yuukiiwa/proactiv:0.1"
     
     input:
     path samplesheet
@@ -20,12 +20,14 @@ process PROACTIV {
     output:    
     path "*.csv"                , emit: proactiv_csv
     path "proactiv.version.txt" , emit: proactiv_version
+    path "dexseq.version.txt"   , emit: dexseq_version
     path "r.version.txt"        , emit: r_version
 
     script:
     """
     run_proactiv.r --samplesheet=$samplesheet
     Rscript -e "library(proActiv); write(x=as.character(packageVersion('proActiv')), file='proactiv.version.txt')"
+    Rscript -e "library(DEXSeq); write(x=as.character(packageVersion('DEXSeq')), file='dexseq.version.txt')"
     echo \$(R --version 2>&1) > r.version.txt
     """
 }
